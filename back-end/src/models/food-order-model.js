fc17078f877d46c10f463818a22f8181a9a8c942
De1oNn <nasanjargal20040524@gmail.com>
2025-02-25 12:33:24 +0800

    commit

--- back-end/src/models/food-order-model.js
+++ back-end/src/models/food-order-model.js
@@ -1,17 +1,23 @@
 import { Schema, model } from "mongoose"
 
-const foodOrderSchema = new Schema({
-  id: { type: Schema.Types.ObjectId },
-  user: { type: Schema.Types.ObjectId },
-  totalPrice: { type: Number, required: true },
-  foodOrderItems: { }, 
-  role: {
-    type: String,
-    enum: ["ADMIN", "USER"],
-    default: "USER"
+const foodOrderSchema = new Schema(
+  {
+    id: { type: Schema.Types.ObjectId },
+    user: { type: Schema.Types.ObjectId },
+    totalPrice: { type: Number, required: true },
+    foodOrderItems: {
+      food: { type: Schema.Types.ObjectId },
+      quantity: { type: Number, required: true }
+    },
+    role: {
+      type: String,
+      enum: ["PENDING", "CANCELED", "DELIVERED"],
+      default: "PENDING",
+    },
   },
-},{
-    timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const foodOrderModel = model("foodOrder", foodOrderSchema)
